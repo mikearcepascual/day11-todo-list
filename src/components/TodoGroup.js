69df43619cd4e8423c6b3409bcf4dc3079ecd86b
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-28 22:28:38 +0800

    refactor: removed console.log in TodoGroup

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -3,8 +3,7 @@ import '../App.css';
 
 const TodoGroup = (props) => {
     const todoItems = [].concat(props.todoList);
-    console.log(todoItems);
-
+    
     return (
     <div className="todoGroup">
         {
