bc3cc64387b35a80e9ce26bff0ca3f7bac1b77fb
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-30 07:46:13 +0800

    refactor: modified TodoGroup to retrieve TodoItems using props from TodoList

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -1,27 +1,26 @@
 import TodoItem from './TodoItem';
-import { useSelector } from "react-redux";
 import '../App.css';
 
-const TodoGroup = () => {
-    const todoItems = useSelector((state) => state.todoList.todoList);
-    if(todoItems.length !== 0){
+const TodoGroup = (props) => {
+    const todoItems = props.todoItems;
+    if (todoItems.length !== 0) {
         return (
-        <div className="todoGroup">
-            {
-                todoItems.map((todoItem, index) => {
-                    return <TodoItem todoItem={todoItem} key={todoItem.id} index={index} />
-                })
-            }
-        </div>
-    )
+            <div className="todoGroup">
+                {
+                    todoItems.map((todoItem, index) => {
+                        return <TodoItem todoItem={todoItem} key={todoItem.id} index={index} />
+                    })
+                }
+            </div>
+        )
     }
-    else{
-        return(
+    else {
+        return (
             <div className="todoGroup">
                 <h2>No Todo Items!</h2>
             </div>
         )
     }
-    
+
 }
 export default TodoGroup;
