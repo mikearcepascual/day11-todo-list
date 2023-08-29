835dc6fcea5761e6008bb98a5d9b9b9a8277f767
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-29 10:17:38 +0800

    refactor: added input validator in submitInput to check if inputItem is blank and added placeholder in input element.

--- src/components/TodoGenerator.js
+++ src/components/TodoGenerator.js
@@ -5,15 +5,18 @@ const TodoGenerator = (props) => {
     const [todoItem, setTodoItem] = useState("");
 
     const submitInput = () => {
-        const inputItem = todoItem;
-        setTodoItem(inputItem);
-        props.onInputChange(inputItem)
-        setTodoItem("");
+        const inputItem = todoItem.trim();
+        if(inputItem !== ""){
+            setTodoItem(inputItem);
+            props.onInputChange(inputItem)
+            setTodoItem("");
+        }
     }
 
     return (
         <div className="todoGenerator">
-            <input type='text' value={todoItem}
+            <input type='text' placeholder="What are you going to do today?" 
+            value={todoItem}
                 onChange={event => setTodoItem(event.target.value)} />
             <button onClick={submitInput}>Add</button>
         </div>
