ea1411be3c772acc85aad94ca77bb8b611848e58
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-30 14:45:35 +0800

    refactor: removed props in DoneGroup

--- src/features/todo/components/DoneGroup.js
+++ src/features/todo/components/DoneGroup.js
@@ -2,7 +2,7 @@ import DoneItem from './DoneItem';
 import { useSelector } from 'react-redux';
 import '../css/app.css';
 
-const DoneGroup = (props) => {
+const DoneGroup = () => {
     const doneItems = useSelector((state) => state.todoList.todoList);
     if (doneItems.length !== 0) {
         return (
