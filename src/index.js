ecaf78f3950b5c58cddc125c916781eb5cb89ece
Mikearce Pascual <PASCUMI3@OOCL.COM>
2023-08-30 11:09:01 +0800

    feat: added react-router-dom dependency

--- src/index.js
+++ src/index.js
@@ -4,13 +4,18 @@ import './index.css';
 import { store } from "./app/store";
 import App from './App';
 import { Provider } from 'react-redux';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App/>
+}]);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router}/>
     </Provider>
   </React.StrictMode>
 );
